36fcf04d798fe6c46999e274a402560a062f06e6
SamuelSantos <samueldamiaosantos@gmail.com>
2023-12-09 23:26:33 +0000

    JS of the website fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
+// making the website dynamic using js
+'use strict'
+
+//variables for navbar menu toggle
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+//variables for navbar search toggle
+const navbarForm = document.querySelector('.navbar-form');
+const navbarFormCloseBtn = document.querySelector('.navbar-form-close');
+const navbarSearchBtn = document.querySelector('.navbar-search-btn');
+
+//variables for menu toggle function
+function navIsActive(){
+    header.classList.toggle('active');
+    nav.classList.toggle('active');
+    navbarMenuBtn.classList.toggle('active');
+}
+
+navbarMenuBtn.addEventListener('click', navIsActive);
+
+
+//navbar search toggle function
+const searchBarIsActive = () => navbarForm.classList.toggle('active');
+
+navbarSearchBtn.addEventListener('click', searchBarIsActive);
+navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
+
+// end of that stuff
 // Define API key and URLs
 const apiKey = 'ae89d8d4072d60d2c5397ecd99f986cb';
 const movieApiUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&api_key=${apiKey}`;
